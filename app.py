96318febcdbc7cfe66bb0e0103511a728c981559
Ousmane Sakho <genieouzog@gmail.com>
2021-04-01 15:22:32 +0000

    requirements fix

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask, request
 import xmltodict, json
 import urllib.request
+import threading
+import time
 
 app = Flask(__name__)
+
+a = { "feed": { "entry": [] } }
+thrs = [];
+result = {}
+def populateLink(objectLink, i, j, link): 
+    populatedObject = urllib.request.urlopen(objectLink).read()
+    # a.append(xmltodict.parse(populatedObject))
+    # a["feed"]["entry"].append(result["feed"]["entry"][i])
+    link["@linkValue"] = xmltodict.parse(populatedObject)
+    # result["feed"]["entry"][i]["link"][j]["@linkValue"] = xmltodict.parse(populatedObject)
 
 @app.route('/')
 def index():
@@ -21,15 +33,22 @@ def getTourrinSoft(name, id):
     contents = urllib.request.urlopen(destinationUrl+"/Objects").read()
     result = xmltodict.parse(contents)
     i= 0 ;
+    print("entress ", len(result["feed"]["entry"]))
+    print("link ", len(result["feed"]["entry"][0]["link"]))
     for entry in result["feed"]["entry"]:
-        if(i < 10):
+        if(i < 100):
             j = 0
             for link in entry["link"]: 
                 if(j > 0): 
-                    populatedObject = urllib.request.urlopen(destinationUrl+"/"+link["@href"]).read()   
-                    link["@linkValue"] = xmltodict.parse(populatedObject)
+                    # populatedObject = urllib.request.urlopen(destinationUrl+"/"+link["@href"]).read()   
+                    # link["@linkValue"] = xmltodict.parse(populatedObject)
+                    t = threading.Thread(target=populateLink, args=(destinationUrl+"/"+link["@href"], i, j, link))
+                    t.start();
+                    thrs.append(t);
                 j = j + 1
         i = i + 1
+    for t in thrs:
+        t.join()
     return result
 
 if __name__ == "__main__":
